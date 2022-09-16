1455ed7d9398f7b24c819e4460ca20dd28f278a1
anand-CodeBin <anand@code-b.in>
2022-09-17 00:54:55 +0530

    Updated the UI

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from urllib import response
 import numpy as np
 from flask import Flask, request, jsonify, render_template
@@ -5,7 +6,6 @@ from pickle import load
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import numpy as np
-from module import ChatBot
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
 
@@ -13,6 +13,7 @@ app = Flask(__name__)
 
 
 # bot=pickle.load(open('bot.pkl','rb'))
+model = load(open("model.pkl", "rb"))
 
 
 @app.route("/")
@@ -25,29 +26,19 @@ def predict():
     """
     For rendering results on HTML GUI
     """
-    # user_input = tokenizer.encode(str(request.form.values())+ tokenizer.eos_token,return_tensors='pt')
-    # prediction= model.generate(user_input,max_length=1000,pad_token_id=tokenizer.eos_token_id)
-    # prediction2 = tokenizer.decode(prediction[:,user_input.shape[-1]:],skip_special_tokens=True)
-    # input=bot.user_input(request.form.values())
-    # response=bot.bot_response(input)
-    print("Request : ", request.form)
-    input_id = request.form.values()
-    encoded = tokenizer.encode(str(input_id) + tokenizer.eos_token, return_tensors="pt")
+    requestData = json.loads(request.data.decode("utf-8"))
+    encoded = tokenizer.encode(
+        str(requestData["text"]) + tokenizer.eos_token,
+        return_tensors="pt",
+    )
     model_rp = model.generate(
         encoded, max_length=1000, pad_token_id=tokenizer.eos_token_id
     )
     response = tokenizer.decode(
-        model_rp[
-            :,
-        ][0],
+        model_rp[:, encoded.shape[-1] :][0],
         skip_special_tokens=True,
     )
-    input_id = ""
-
-    return "DATA WILL BE HERE!!!"
-
-    # return render_template('index.html', prediction_text='Response {}'.format(response))
+    return "".join(response)
 
 
 app.run(debug=True)
-model = load(open("model.pkl", "rb"))
